0e391ab73cc57a16efef1694b8c1122dff3a120c
Vinay Patel <nitianvinaypatel@gmail.com>
2026-07-31 05:15:19 +0000

    Project Setup

---
+++ server/src/main/java/com/payment/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.payment.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
